c85a30beb8d92cabb51a37f380f2d136f2182084
Sergio Arroyo <serarroy@ucm.es>
2021-03-02 20:35:33 +0100

    MassLossingBody hecho

---
+++ Simulador fisico/src/simulator/model/MassLossingBody.java
@@ -0,0 +1,40 @@
+package simulator.model;
+import simulator.misc.Vector2D;
+
+public class MassLossingBody extends Body{
+
+	private double lossFactor;
+	private double lossFrequency;
+	
+	MassLossingBody(String id, double m, Vector2D vel, Vector2D pos, Vector2D ace, double lFactor, double lFreq) {
+		super(id, m, vel, pos, ace);
+		this.lossFactor = lFactor;
+		this.lossFrequency = lFreq;
+	}
+
+	public double getLossFactor() {
+		return lossFactor;
+	}
+
+	public double getLossFrequency() {
+		return lossFrequency;
+	}
+	
+	public void setLossFactor(double lossFactor) {
+		this.lossFactor = lossFactor;
+	}
+
+	public void setLossFrequency(double lossFrequency) {
+		this.lossFrequency = lossFrequency;
+	}
+	
+	public void move(double t) {
+		super.move(t);
+		double c = 0.0;	
+		c += t;
+		if(c >= this.lossFrequency) {
+			this.m *= (1 - this.lossFactor);
+			c = 0.0;
+		}
+	}
+}
